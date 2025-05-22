e5637226fed3f168ca26c6268a2d9051c47a06ab
Pranab-kr <pranabm406@gmail.com>
2025-05-23 00:19:29 +0530

    Update app.js

--- swiper_01/app.js
+++ swiper_01/app.js
@@ -25,40 +25,40 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
-  document.addEventListener("DOMContentLoaded", function () {
-    particlesJS("particles-js", {
-      particles: {
-        number: { value: 160, density: { enable: true, value_area: 800 } },
-        color: { value: ["#ffffff", "#192bce", "#08a06b"] },
-        shape: { type: "circle" },
-        opacity: {
-          value: 0.6,
-          random: false,
-          anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false },
-        },
-        size: {
-          value: 5,
-          random: true,
-          anim: { enable: true, speed: 4, size_min: 0.1, sync: false },
-        },
-        line_linked: {
-          enable: true,
-          distance: 150,
-          color: "#ffffff",
-          opacity: 0.4,
-          width: 1,
-        },
-        move: {
-          enable: true,
-          speed: 1,
-          direction: "none",
-          random: true,
-          straight: false,
-          out_mode: "out",
-          bounce: false,
-          attract: { enable: false, rotateX: 600, rotateY: 1200 },
-        },
+document.addEventListener("DOMContentLoaded", function () {
+  particlesJS("particles-js", {
+    particles: {
+      number: { value: 160, density: { enable: true, value_area: 800 } },
+      color: { value: ["#ffffff", "#192bce", "#08a06b"] },
+      shape: { type: "circle" },
+      opacity: {
+        value: 0.6,
+        random: false,
+        anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false },
       },
-      retina_detect: true,
-    });
+      size: {
+        value: 5,
+        random: true,
+        anim: { enable: true, speed: 4, size_min: 0.1, sync: false },
+      },
+      line_linked: {
+        enable: true,
+        distance: 150,
+        color: "#ffffff",
+        opacity: 0.4,
+        width: 1,
+      },
+      move: {
+        enable: true,
+        speed: 1,
+        direction: "none",
+        random: true,
+        straight: false,
+        out_mode: "out",
+        bounce: false,
+        attract: { enable: false, rotateX: 600, rotateY: 1200 },
+      },
+    },
+    retina_detect: true,
   });
+});
